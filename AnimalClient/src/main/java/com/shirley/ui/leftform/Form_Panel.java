3ec860115dd984b5806bb8e5bc4935ddafd2c00c
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-08 10:37:23 -0600

    building ui

--- AnimalClient/src/main/java/com/shirley/ui/leftform/Form_Panel.java
+++ AnimalClient/src/main/java/com/shirley/ui/leftform/Form_Panel.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.shirley.ui.leftform;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 /**
  *
  * @author Chantal Shirley
  */
 public class Form_Panel {
-    
+    private JLabel title_label;
+    private JLabel find_label;
+    private JTextField find_textfield;
+    private JButton lookup_button;
+    private JButton exit_button;
+    private FormListener formListener;
 }
