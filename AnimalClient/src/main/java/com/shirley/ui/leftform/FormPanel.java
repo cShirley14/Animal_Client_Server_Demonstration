44cb851794d88a163cbe255913e3a749ba11df57
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-08 10:40:31 -0600

    fixed bugs

--- AnimalClient/src/main/java/com/shirley/ui/leftform/FormPanel.java
+++ AnimalClient/src/main/java/com/shirley/ui/leftform/FormPanel.java
@@ -1,5 +1,6 @@
 package com.shirley.ui.leftform;
 
+import com.shirley.ui.Languages;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -20,11 +21,25 @@ import javax.swing.border.Border;
  *
  * @author Chantal Shirley
  */
-public class Form_Panel {
+public class FormPanel extends JPanel {
     private JLabel title_label;
     private JLabel find_label;
     private JTextField find_textfield;
     private JButton lookup_button;
     private JButton exit_button;
     private FormListener formListener;
+    private ResourceBundle messages = Languages.getResourceBundle();
+
+    public FormPanel() {
+        Dimension size = getPreferredSize();
+        size.width = 500;
+        setPreferredSize(size);
+        
+        Border inner_border = BorderFactory.createTitledBorder(
+                messages.getString("title"));
+        Border outer_border = BorderFactory.createEmptyBorder(5,5,5,5);
+        setBorder(BorderFactory.createCompoundBorder(outer_border, 
+                inner_border));
+    }
+    
 }
