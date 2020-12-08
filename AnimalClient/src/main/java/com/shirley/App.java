c7260def8c834163d1c4347bae7db40f9ec74740
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-08 10:04:56 -0600

    Small code changes

--- AnimalClient/src/main/java/com/shirley/App.java
+++ AnimalClient/src/main/java/com/shirley/App.java
@@ -1,5 +1,9 @@
 package com.shirley;
 
+import com.shirley.ui.Languages;
+import com.shirley.ui.MainFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author Chantal Shirley
@@ -10,14 +14,12 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
         Languages.setResourceBundle(Languages.LanguageOptions.USA);
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new MainFrame();
             }
         });
-        */
     }
     
 }
