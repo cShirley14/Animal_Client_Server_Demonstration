c80c462683bb0aa7e1de131fb17b196ad78cb190
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-08 14:29:37 -0600

    More sql

--- Animal_Database.sql
+++ Animal_Database.sql
@@ -8,9 +8,6 @@ Database used to catalogue animals.
 ***************************************/
 
 /** Needed operations
-* Drop/Create Database
-* Drop/Create Table
-* Drop/Create Procedure to add an Animal record
 * Drop/Create Procedure to get a single Animal Record
 * Call procedure to add 3-5 animals
 **/
@@ -25,25 +22,25 @@ CREATE DATABASE animal;
 -- Select the database
 USE animal;
 
---drop any existing table
-DROP TABLE animal;
 -- create table
 CREATE TABLE animal
 (
     id                      VARCHAR(25) PRIMARY KEY,
-    animal_name             VARCHAR(150) NOT NULL,
+    name             		VARCHAR(150) NOT NULL,
+    species                 ENUM('Dog', 'Cat', 'Unknown') NOT NULL DEFAULT 'Unknown',
     gender                  ENUM('Male', 'Female', 'Unknown') NOT NULL DEFAULT 'Unknown',
     age                     SMALLINT UNSIGNED NOT NULL,
     fixed                   ENUM('Yes', 'No') DEFAULT 'No',
     legs                    INT UNSIGNED NOT NULL,
-    weight                  FLOAT (8,4) NOT NULL,
+    weight                  FLOAT (8,2) NOT NULL,
     dateAdded               DATE NOT NULL,
     lastFeedingTime         DATETIME
 );
 
 INSERT INTO animal (
     id
-    , animal_name
+    , name
+    , species    
     , gender
     , age
     , fixed
@@ -54,11 +51,63 @@ INSERT INTO animal (
 ) VALUES (
     '18'
     , 'Aech'
+    , 'Dog'
     , 'Male'
     , 2
     , 'Yes'
     , 4
     , 55
     , '2018-12-25'
-    , '2020-12-24'
+    , '2020-12-24 11:30:27'
+),
+(
+    '68'
+    , 'Buster'
+    , 'Dog'
+    , 'Male'
+    , 4
+    , 'Yes'
+    , 4
+    , 75
+    , '2016-04-05'
+    , '2020-12-24 15:24:45'
+),
+(
+    '12863'
+    , 'Farrow'
+    , 'Cat'
+    , 'Female'
+    , 1
+    , 'Yes'
+    , 4
+    , 8.5
+    , '2017-02-13'
+    , '2020-12-24 18:45:32'
+),
+(
+    '15'
+    , 'Totoro'
+    , 'Cat'
+    , 'Male'
+    , 9
+    , 'Yes'
+    , 4
+    , 17.12
+    , '2020-05-27'
+    , '2020-12-24 06:24:17'
+),
+(
+    '14'
+    , 'Unknown'
+    , 'Unknown'
+    , 'Unknown'
+    , 5
+    , 'No'
+    , 4
+    , 5
+    , '2020-11-16'
+    , '2020-12-24 04:17:35'
 );
+
+
+select * from animal;
